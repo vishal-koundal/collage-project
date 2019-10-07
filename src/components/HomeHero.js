a011c6b3042cbfe3fc6bf4cf525f0b284af34688
vishal-koundal <vishal2661999@gmail.com>
2019-10-07 02:37:13 -0700

    add hero

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 import dayjs from 'dayjs';
+import { Button, Subtitle, Heading } from './elements';
 
 const Container = styled.div`
   background: url(https://colorlib.com/preview/theme/coffee/img/header-bg.jpg);
@@ -9,8 +10,16 @@ const Container = styled.div`
 `;
 
 const HomeHero = () => (
-  <Container className="pt-64 pb-64">
-    
+  <Container className="pt-32 pb-64 px-6 sm:px-24 bg-center">
+    <div className="w-full lg:w-1/2">
+      <Subtitle white className="uppercase font-bold pb-4">
+        Now you can feel the Energy.
+      </Subtitle>
+      <Heading white className="text-3xl sm:text-6xl font-bold pb-4">
+        Start your day with a black Coffee
+      </Heading>
+      <Button>BUYmdkfjh</Button>
+    </div>
   </Container>
 );
 
---
+++ src/components/elements/index.js
@@ -0,0 +1,6 @@
+import Button from './Button';
+import Heading from './Heading';
+import Title from './Title';
+import Subtitle from './Subtitle';
+
+export { Button, Heading, Title, Subtitle };
