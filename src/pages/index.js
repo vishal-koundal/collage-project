78247a3ce2d8106c1664c5acff9aeff7c4ee2cd0
Vishal <50748766+vishal-koundal@users.noreply.github.com>
2019-12-31 17:42:25 +0530

    minor fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <Seo title="Home" description="Welcome to GatsbyJs v1" />
+        <Seo title="Home" description="Welcome to CafeCoffee" />
         <Container>
           <HomeHero />
           <HomeAbout />
